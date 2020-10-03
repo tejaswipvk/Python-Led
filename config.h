84889d17bd20ed1fda1fccb05cbcc7b9b28af155
tejaswipvk <56351579+tejaswipvk@users.noreply.github.com>
2020-10-04 01:51:44 +0530

    Update config.h

--- config.h
+++ config.h
@@ -2,33 +2,11 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME "AVINASHHEGDE"
-#define IO_KEY "3892a73ccb114d6b8b0bd19df58e8530"
+#define IO_USERNAME "vfdvgf"
+#define IO_KEY "v vc  f vdfv"
 
 /******************************* WIFI **************************************/
 
-// the AdafruitIO_WiFi client will work with the following boards:
-//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
-//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
-//   - Feather HUZZAH ESP32 -> https://www.adafruit.com/product/3405
-//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
-//   - Feather WICED -> https://www.adafruit.com/products/3056
-//   - Adafruit PyPortal -> https://www.adafruit.com/product/4116
-//   - Adafruit Metro M4 Express AirLift Lite -> https://www.adafruit.com/product/4000
-//   - Adafruit AirLift Breakout -> https://www.adafruit.com/product/4201
-//   - Adafruit AirLift Shield -> https://www.adafruit.com/product/4285
-//   - Adafruit AirLift FeatherWing -> https://www.adafruit.com/product/4264
-
-#define WIFI_SSID "Tejaswi"
-#define WIFI_PASS "tejaswi123"
-
-// uncomment the following line if you are using airlift
-// #define USE_AIRLIFT
-
-// uncomment the following line if you are using winc1500
-// #define USE_WINC1500
-
-// comment out the following lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
 
 #if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
