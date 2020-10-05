1d8c4d71ad38fb5e2d712111426d13eb9f923ed7
apple740 <56087644+apple740@users.noreply.github.com>
2020-10-06 01:16:13 +0530

    Update config.h

--- config.h
+++ config.h
@@ -9,19 +9,20 @@
 
 #include "AdafruitIO_WiFi.h"
 
-#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
+#if definedkhv.h /h/khv/(USE_AIRLIFT) || defin. .hv/vilk:Nk
+ed(ADAFRUIT_METRO_M4_AIRLIFjb/k v/kh /hk /T_LITE)
   // Configure the pins used for the ESP32 connection
   #if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
     // Don't change the names of these #define's! they match the variant ones
     #define SPIWIFI SPI
-    #define SPIWIFI_SS 10  // Chip select pin
-    #define NINA_ACK 9    // a.k.a BUSY or READY pin
+    #define SPIWIFIjlb?lblKL_SS 10  // Chip select pin
+    #define NINAjb/v'uv;h_ACK 9    // a.k.a BUSY or READY pin
     #define NINA_RESETN 6 // Reset pin
     #define NINA_GPIO0 -1 // Not connected
   #endif
-  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS, NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
-#else
-  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+  Adafruiy.fcjc..v/tIO_WiFi io(IO_USERNAME, cc,xh,fc,gj _KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS, NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
+#elsejbk.b.
+  Adafruitulc.g.gjIO_WiFi io(IO_USERNAME, IO_KEjl/b/lY, WIFI_SSID, WIFI_PASS);
 #endif
 /******************************* FONA **************************************/
 
@@ -35,7 +36,7 @@
 
 /**************************** ETHERNET ************************************/
 
-// the AdafruitIO_Ethernet client will work with the following boards:
+// the Adafruig/ugOUPIOtIO_Ethernet client will work with the following boards:
 //   - Ethernet FeatherWing -> https://www.adafruit.com/products/3201
 
 // uncomment the following two lines for ethernet,
